c328cb3be7634bbfbf5df83bf0a0425eaafc074f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-22 17:37:06 +0000

    automatically backed up by learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,9 +1,19 @@
 require_relative "./vehicle.rb"
 class Car < Vehicle
-  attr_accessor :model
+  attr_accessor :wheel_size, :wheel_number
 
   def initialize
-    @model = model
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
   end
 
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+  
 end
+
